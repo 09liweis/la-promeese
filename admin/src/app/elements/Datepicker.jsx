62d1aaeffa6a22def6041a5fd23fbd580a4972f2
samliweisen <liweisen007@gmail.com>
2017-12-28 16:17:14 +0000

    fix calendar show negative value

--- admin/src/app/elements/Datepicker.jsx
+++ admin/src/app/elements/Datepicker.jsx
@@ -19,8 +19,9 @@ class Datepicker extends React.Component {
         this.props.handleChange(d);
     }
     render() {
+        const value = this.props.value == '0000-00-00' ? '' : this.props.value;
         return (
-            <Flatpickr className="input" placeholder="Please select a date" options={{dateFormat: 'Y-m-d'}} name={this.props.name} value={this.props.value} onChange={this.handleChange} />
+            <Flatpickr className="input" placeholder="Please select a date" options={{dateFormat: 'Y-m-d'}} name={this.props.name} value={value} onChange={this.handleChange} />
         );
     }
 }
