4c54e3b0e4645de5f2000496da78ab1adbba03cf
samliweisen <liweisen007@gmail.com>
2018-01-11 17:18:38 +0000

    update js version

--- admin/index.php
+++ admin/index.php
@@ -11,6 +11,6 @@
     </head>
     <body>
         <div id="app"></div>
-        <script type="text/javascript" src="/admin/src/public/bundle.js?v=5.9.5.3"></script>
+        <script type="text/javascript" src="/admin/src/public/bundle.js?v=5.9.5.4"></script>
     </body>
 </html>
