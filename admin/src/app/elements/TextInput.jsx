5cb69eb2e02754c16de6f1fe0bfb112f7c4bfcd2
samliweisen <liweisen007@gmail.com>
2018-01-13 23:01:30 +0000

    put input mask to textinput component

--- admin/src/app/elements/TextInput.jsx
+++ admin/src/app/elements/TextInput.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import InputMask from 'react-input-mask';
 
 class TextInput extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
+        let inputField = <input className="input" type="text" name={this.props.name} value={this.props.value} onChange={this.props.handleChange} />;
+        if (this.props.type == 'phone') {
+            inputField = <InputMask className="input" name={this.props.name} value={this.props.value} mask="999-999-9999" placeholder="123-456-7890" maskChar=" "  onChange={this.props.handleChange} />;
+        }
         return (
             <div className="field">
                 <label className="label">{this.props.title}</label>
                 <div className="control">
-                    <input className="input" type="text" name={this.props.name} value={this.props.value} onChange={this.props.handleChange} />
+                    {inputField}
                 </div>
             </div>
         );
